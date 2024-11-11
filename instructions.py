17831760f74ab5feee2edbe09902d7e2a7428384
b0gd1i <fogarasi_bogdan@yahoo.com>
2024-11-11 19:30:20 +0200

    Create instructions.py

---
+++ instructions.py
@@ -0,0 +1,7 @@
+# 1. log into instagram on your firefox browser
+# 2. run the session grabber
+# 3. if the session grab was sucessful, run the follower grabber
+# 4. write the name of the account that you want to get the info off. NOTE: it must be a public account, if it's private you must be a follower
+# 5. wait for a bit. the code runs a bit slow so your instagram account will not get suspended for autoamtic behaviour
+# 6. when the code has finished running, the paths to the files will be printed in the console
+# 7. enjoy
